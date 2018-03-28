2072f9cdfe28959cc1cc868b77c748dccb9585ae
Jeremy Weatherford <jweather@xidus.net>
2018-03-28 17:46:10 -0400

    center section of new stage design

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,8 +1,22 @@
 #include "ofApp.h"
 
-#define NRECT	160
+#define NRECT	63
+#define NPART	(NRECT*4)
 
-#define NPART	(NRECT*8)
+const int screenW = 1024, screenH = 768;
+
+const int width = 80;
+const int height = 80;
+const int gapX = 5;
+const int gapY = 5;
+
+const int xstart = (screenW - (7 * width)) / 2;
+const int ystart = (screenH - (9 * height)) / 2;
+
+const int Cx = (80 * 7) / 2 + xstart;
+const int Cy = (80 * 9) / 2 + ystart;
+
+
 struct part {
 	bool active;
 	double x, y, theta;
@@ -15,7 +29,7 @@ int nextPart = 0;
 const double gravity = 0.01;
 
 // viewport
-int vx = 43, vy = 290, vw = 967, vh = 726;
+int vx = 0, vy = 0, vw = 1024, vh = 768;
 
 #define NPAL	32
 ofColor palette[NPAL];
@@ -46,7 +60,7 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-int cx = 450, cy = 225;
+int cx = Cx, cy = Cy;
 bool orbit = false;
 float theta = 0;
 float bumpAmt = 0;
@@ -62,7 +76,6 @@ int shatter = 0;
 bool borderChase = false;
 bool hypnotize = false;
 bool showID = false;
-bool square = false;
 
 float borderPhase = 0.0;
 float borderStep = 0.001;
@@ -106,7 +119,7 @@ void ofApp::update(){
 
 	// palette shift
 	if ((ofGetFrameNum() % 10) == 0) {
-		int w = (square ? 20 : 10);
+		int w = 7;
 		switch (palMode) {
 		case 0: // linear
 			for (int i = NRECT - 1; i > 0; i--) {
@@ -143,12 +156,12 @@ void ofApp::update(){
 	// orbit center point
 	theta += 0.005;
 	if (orbit) {
-		cx = 450 + 100 * sin(theta);
-		cy = 225 + 100 * cos(theta);
+		cx = Cx + 100 * sin(theta);
+		cy = Cy + 100 * cos(theta);
 	}
 	else {
-		cx = 450;
-		cy = 225;
+		cx = Cx;
+		cy = Cy;
 	}
 }
 
@@ -187,8 +200,8 @@ void drawRect(int x1, int y1, int x2, int y2, ofColor fillColor) {
 		distort(&x[i], &y[i]);
 
 	if (shatter == 1) {
-		// break each rect into 8x particles
-		for (int px = 0; px < 4; px++) {
+		// break each rect into 4x particles
+		for (int px = 0; px < 2; px++) {
 			for (int py = 0; py < 2; py++) {
 				parts[nextPart].x = x[0] + 20 + px * 20;
 				parts[nextPart].y = y[0] + 20 + py * 20;
@@ -244,7 +257,7 @@ void drawRect(int x1, int y1, int x2, int y2, ofColor fillColor) {
 
 	//hypnotize
 	if (hypnotize) {
-		for (int d = 2 + (ofGetFrameNum()/10) % 10; d < 40; d += 10) {
+		for (int d = 2 + (ofGetFrameNum()/20) % 10; d < 20; d += 5) {
 			ofSetColor(255 - d*6);
 			ofDrawLine(x1 + d, y1 + d / 2, x2 - d, y1 + d / 2);
 			ofDrawLine(x2 - d, y1 + d / 2, x2 - d, y2 - d / 2);
@@ -263,27 +276,37 @@ void drawRect(int x1, int y1, int x2, int y2, ofColor fillColor) {
 }
 
 //--------------------------------------------------------------
+bool hide(int x, int y) {
+	if (y == 0 && x != 3) return true;
+	if (y == 1 && (x < 2 || x > 4)) return true;
+	if (y == 2 && (x < 1 || x > 5)) return true;
+	return false;
+}
+
 void ofApp::draw(){
 	ofClear(ofColor(0, 0, 0));
 	ofViewport(vx, vy, vw, vh);
 
 	if (shatter < 2) {
 		int rIndex = 0;
-		bool xShift = false;
-		int xstep = square ? 50 : 100;
-		int width = square ? 40 : 90;
-		for (int y = 10; y < 400; y += 50) {
-			for (int x = 10; x < 1000; x += xstep) {
-				int xx = x + (xShift ? 50 : 0);
-				drawRect(xx, y, xx + width, y + 45, palette[palIndex[rIndex]]);
+
+		for (int x = 0; x < 7; x++) {
+			for (int y = 0; y < 9; y++) {
+				if (hide(x, y)) {
+					rIndex++;
+					continue;
+				}
+				int xx = xstart + x * width;
+				int yy = ystart + y * height;
+
+				drawRect(xx, yy, xx + width-gapX, yy + height-gapY, palette[palIndex[rIndex]]);
 				
 				if (showID) {
 					ofSetColor(0, 255, 0);
-					font.drawString(to_string(rIndex), xx + 50, y + 35);
+					font.drawString(to_string(rIndex), xx + 10, yy + 25);
 				}
 				rIndex++;
 			}
-			if (!square) xShift = !xShift;
 		}
 		if (shatter == 1) {
 			nextPart = 0;
@@ -306,9 +329,11 @@ void ofApp::draw(){
 	}
 
 	// mask
+	/*
 	ofSetColor(0, 0, 0);
 	ofDrawRectangle(0, 0, 59, 1000);
 	ofDrawRectangle(951, 0, 1000, 1000);
+	*/
 }
 
 //--------------------------------------------------------------
@@ -348,9 +373,7 @@ void ofApp::keyPressed(int key){
 		break;
 	case 'i':
 		showID = !showID;
-		break;
-	case 'q':
-		square = !square;
+		cout << "show ID " << showID << endl;
 		break;
 	case 'm':
 		palMode = (palMode + 1) % NPALMODES;
@@ -401,20 +424,24 @@ void ofApp::keyPressed(int key){
 	case OF_KEY_DOWN:
 	case OF_KEY_LEFT:
 	case OF_KEY_RIGHT:
+		int inc = 1;
+		if ((GetKeyState(VK_CONTROL) & 0x80) == 0x80) {
+			inc = 10;
+		}
 		if ((GetKeyState(VK_SHIFT)&0x80) == 0x80) {
 			switch (key) {
-			case OF_KEY_UP:	vh--; break;
-			case OF_KEY_DOWN: vh++; break;
-			case OF_KEY_LEFT: vw--; break;
-			case OF_KEY_RIGHT: vw++; break;
+			case OF_KEY_UP:	vh-=inc; break;
+			case OF_KEY_DOWN: vh+=inc; break;
+			case OF_KEY_LEFT: vw-=inc; break;
+			case OF_KEY_RIGHT: vw+=inc; break;
 			}
 		}
 		else {
 			switch (key) {
-			case OF_KEY_UP:	vy--; break;
-			case OF_KEY_DOWN: vy++; break;
-			case OF_KEY_LEFT: vx--; break;
-			case OF_KEY_RIGHT: vx++; break;
+			case OF_KEY_UP:	vy-=inc; break;
+			case OF_KEY_DOWN: vy+=inc; break;
+			case OF_KEY_LEFT: vx-=inc; break;
+			case OF_KEY_RIGHT: vx+=inc; break;
 			}
 		}
 
